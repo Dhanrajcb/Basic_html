ee70912d40c40cb61d218d2efe4c3349d1ebe9c4
Dhanraj <dhanrajbhandge00@gmail.com>
2025-05-16 15:55:38 +0530

    updated

---
+++ java_script/1.js
@@ -0,0 +1,139 @@
+// var fullname="BCA Project";
+// age=23;
+// year=2025;
+// console.log(fullname + " is a project of BCA");
+// console.log("I am " + age + " years old");
+// console.log("I will complete my BCA in " + year);
+
+// using var
+
+// var username="BCA Project";
+// var username="Dhanraj";
+// var Username="Dhanraj Kumar";
+// console.log(Username);
+
+// using let
+
+// let a=10;
+// age=40;
+// console.log(age);
+
+
+// let age;
+// console.log(age);
+
+// {
+//     let a=10;
+//     console.log(a);
+// }
+
+// {
+//     let a=20;
+//     console.log(a);
+// }
+
+// const keyword
+
+// const a=10.77;
+// console.log(a);
+
+// const TAX_RATE=0.18;
+// const baseprice=100;
+// const finalPrice=baseprice + (baseprice * TAX_RATE);
+// console.log("Final price is " + finalPrice);
+
+// Data Types
+
+// string
+
+// let name="Dhanraj";
+// console.log(name);
+
+// number
+// let age=23;
+// console.log(age)
+
+// boolean
+// let isStudent=true;
+// let isTeacher=false;
+// console.log(isTeacher);
+// console.log(isStudent);
+
+// null
+// let car=null;
+// console.log(car);
+
+// undefined
+// let x;
+// console.log(x);
+
+// symbol
+// let sym=Symbol("This is a symbol");
+// console.log(sym);
+
+// BigInt
+// let bigInt=BigInt(1234567890123456789012345678901234567890);
+// console.log(bigInt);
+
+// single line comment
+// this is a single line comment
+
+// multi line comment
+/*
+this is a multi line comment    
+*/
+
+// operators
+
+// arithmetic operators
+// let a=10;
+// let b=20;
+// console.log("a + b =",a+b);
+// console.log("a - b =",a-b);
+// console.log("a * b =",a*b);
+// console.log("a / b =",a/b);
+// console.log("a % b =",a%b);
+// console.log("a ** b =",a**b);
+
+// Assignment operators
+// a+=b;
+// console.log("a += b =",a);
+// a-=b;
+// console.log("a -= b =",a);
+// a*=b;
+// console.log("a *= b =",a);
+// a/=b;
+// console.log("a /= b =",a);
+// a%=b;
+// console.log("a %= b =",a);
+// a**=b;
+// console.log("a **= b =",a);
+
+// Exponential operator
+// let a=2;
+// let b=3;
+// console.log("a**b =",a**b);
+
+// Increment and decrement operators
+// let a=10;
+// console.log("++a =",++a);
+// console.log("--a =",--a);
+
+// camparison operators
+// let a=10;
+// let b=20;
+// console.log("a == b",a==b);
+// console.log("a != b",a!=b);
+// console.log("a<b =",a<b);
+// console.log("a>b =",a>b);
+// console.log("a<=b =",a<=b);
+// console.log("a>=b =",a>=b);
+
+// logical operators
+let c=25;
+let d=10;
+let e=c + d== 15;
+let f=c + d != 15;
+console.log("e&&f =",e&&f);
+console.log("e||f =",e||f);
+console.log("!e =",!e);
